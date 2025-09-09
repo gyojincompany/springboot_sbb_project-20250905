4a856c93107a4487dbc1cdd242b3084e2f52f72a
gyojincompany <luckyred@gmail.com>
2025-09-09 11:10:40 +0900

    더미 데이터 300개 생성 테스트 케이스 작성

---
+++ src/test/java/com/gyojincompany/gyojinboard/DummyDataInputTest.java
@@ -0,0 +1,25 @@
+package com.gyojincompany.gyojinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.gyojincompany.gyojinboard.question.QuestionService;
+
+@SpringBootTest
+public class DummyDataInputTest {
+	
+	@Autowired
+	private QuestionService questionService;
+	
+	@Test
+	public void testDummy() {
+		for(int i=1;i<=300;i++) { //300개의 질문 더미 데이터 생성
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "연습 내용 더미데이터 입니다.";
+			questionService.create(subject, content);
+		}
+		
+	}
+
+}
