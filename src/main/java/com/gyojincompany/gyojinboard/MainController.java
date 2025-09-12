0918048f6f4e240d9a5f6611b3f7851cd7fee633
gyojincompany <luckyred@gmail.com>
2025-09-12 15:07:11 +0900

    루트 링크 수정 추가

--- src/main/java/com/gyojincompany/gyojinboard/MainController.java
+++ src/main/java/com/gyojincompany/gyojinboard/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
-	@GetMapping(value = "/gyojincomboard") //오라클 클라우드 root 요청 처리
+	@GetMapping(value = "/") //오라클 클라우드 root 요청 처리
 	//@GetMapping(value = "/") //로컬용 root 요청 처리
 	public String root() {
 		return "redirect:/question/list";
