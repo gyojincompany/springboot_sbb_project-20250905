486af432f8cc84ace9c849f725c9d0f3840efefc
gyojincompany <luckyred@gmail.com>
2025-09-12 14:09:29 +0900

    Application.java 파일 수정 추가

--- src/main/java/com/gyojincompany/gyojinboard/GyojincompanyProjectApplication.java
+++ src/main/java/com/gyojincompany/gyojinboard/GyojincompanyProjectApplication.java
@@ -2,9 +2,18 @@ package com.gyojincompany.gyojinboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GyojincompanyProjectApplication {
+public class GyojincompanyProjectApplication extends SpringBootServletInitializer{
+	
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(GyojincompanyProjectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GyojincompanyProjectApplication.class, args);
